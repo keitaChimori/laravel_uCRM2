460d4a0708d5562112e4c76f91c17f3e5b2c1214
keitaChimori <keitachimori.1220@gmail.com>
2023-12-06 20:50:38 +0900

    セクション2

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+  id: String,
+  blog: Object,
+});
+
+// const deleteConfirm = id => {
+//   // console.log(id);
+//   Inertia.delete(`/inertia/${id}`, {
+//     onBefore: () => confirm('本当に削除しますか？')
+//   })
+// }
+
+const deleteConfirm = id => {
+  // console.log(id);
+  Inertia.delete(route('inertia.delete', id), {
+    onBefore: () => confirm('本当に削除しますか？')
+  })
+}
+
+</script>
+<template>
+  ID:{{ id }}<br>
+  タイトル:{{ blog.title }}<br>
+  本文:{{ blog.content }}<br>
+  <button @click="deleteConfirm(blog.id)">削除</button>
+</template>
